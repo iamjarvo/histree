3b0ed053b5aaf00471069cadb8bd8d18dafc4dc9
Jearvon Dharrie <j.dharrie@gmail.com>
2015-03-12 18:55:12 -0400

    Add version flag

--- lib/histree.ex
+++ lib/histree.ex
@@ -1,4 +1,14 @@
 defmodule Histree.CLI do
   def main(args) do
+    args |> process_args
+  end
+
+  def process_args(args) do
+    options = OptionParser.parse(args)
+
+    case options do
+      { [version: true], _, _ } -> IO.puts "Version 0.0.1"
+      _ -> IO.puts "sup yall"
+    end
   end
 end
--- test/features/cli.bats
+++ test/features/cli.bats
@@ -4,3 +4,8 @@
   run ./histree
   [ $status -eq 0 ]
 }
+
+@test "it returns the version with the --version flag" {
+  run ./histree --version
+  echo $output | grep "Version"
+}
